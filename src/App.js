a8c6268f72789381e85aba2ac1d554600d437fb1
brightuautumnsky.21 <brightautumnsky.21@gmail.com>
2022-04-10 17:15:45 +0900

    state management with context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useReducer, useRef } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Journal from "./Pages/Journal";
@@ -8,27 +9,91 @@ import Button from "./Components/Button";
 import Header from "./Components/Header";
 import RouteLinkTest from "./Components/RouteLinkTest";
 
+const reducer = (state, action) => {
+  let newState = [];
+
+  switch (action.type) {
+    case "INIT":
+      return action.data;
+    case "CREATE":
+      newState = [...state, action.data];
+      break;
+    case "DELETE":
+      newState = state.filter((item) => item.id !== action.targetId);
+      break;
+    case "EDIT":
+      newState = state.map((item) =>
+        item.id === action.data.id ? { ...action.data } : item
+      );
+      break;
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const JournalStateContext = React.createContext();
+export const JournalDispatchContext = React.createContext();
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({ type: "DELETE", targetId });
+  };
+
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: { id: targetId, date: new Date(date).getTime(), content, emotion },
+    });
+  };
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header
-          text="header"
-          leftChild={
-            <Button text="left" type="positive" onClick={() => alert("left")} />
-          }
-          rightChild={<Button text="right" onClick={() => alert("right")} />}
-        />
-        {/* 라우팅 */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/journal/:id" element={<Journal />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-        </Routes>
-        <RouteLinkTest />
-      </div>
-    </BrowserRouter>
+    <JournalStateContext.Provider value={data}>
+      <JournalDispatchContext.Provider value={{ onCreate, onDelete, onEdit }}>
+        <BrowserRouter>
+          <div className="App">
+            <Header
+              text="header"
+              leftChild={
+                <Button
+                  text="left"
+                  type="positive"
+                  onClick={() => alert("left")}
+                />
+              }
+              rightChild={
+                <Button text="right" onClick={() => alert("right")} />
+              }
+            />
+            {/* 라우팅 */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/journal/:id" element={<Journal />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+            </Routes>
+            <RouteLinkTest />
+          </div>
+        </BrowserRouter>
+      </JournalDispatchContext.Provider>
+    </JournalStateContext.Provider>
   );
 }
 
